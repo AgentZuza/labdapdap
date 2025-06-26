2ca0bdf47c979448eeddfc9483bca7de4afc52dd
AgentZuza <ts3779791@gmail.com>
2025-06-27 04:55:07 +0700

    Updated by love_gitler

--- loader.lua
+++ loader.lua
@@ -1,7 +1,8 @@
 local users = {
     {username = "test", password = "test", discord_nickname = "", hwid = "", subscription = "true" },
     {username = "ch111", password = "ch111", discord_nickname = "trlessless", hwid = "B189165D-B889-4FD5-BE1A-AB0B6C4FE674", subscription = "true"},
-    {username = "admin", password = "admin123", discord_nickname = "love_gitler", hwid = "95DC1493-4ED1-436F-B563-CC545354CD8B", subscription = "true" }
+    {username = "admin", password = "admin123", discord_nickname = "love_gitler", hwid = "95DC1493-4ED1-436F-B563-CC545354CD8B", subscription = "true" },
+    {username = "sdasdas", password = "sadasdasd", discord_nickname = "love_gitler", discord_id = "1322273544205504542", hwid = "", subscription = "false"}
 }
 
 
