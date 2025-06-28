a3dd417eb52a74e7a29ae2813388f6299dd822d5
AgentZuza <ts3779791@gmail.com>
2025-06-28 21:43:54 +0700

    Updated loader.lua by love_gitler

--- loader.lua
+++ loader.lua
@@ -346,7 +346,7 @@ LoginButton.MouseButton1Click:Connect(function()
     else
         if checkUser(username, password) then
             LoaderGui:Destroy()
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentZuza/labdapdap/refs/heads/main/pumpum.lua"))()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentZuza/labdapdap/refs/heads/main/pumpum_v0.lua"))()
         else
             shakeMainFrame()
         end
