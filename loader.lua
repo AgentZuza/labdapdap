954340fb960c0d15ca5df0d9045043594b0ea657
AgentZuza <ts3779791@gmail.com>
2025-06-30 12:53:29 +0700

    Updated loader.lua by byblik_v_smetane

--- loader.lua
+++ loader.lua
@@ -1,7 +1,8 @@
 local users = {
     {username = "test", password = "test", discord_nickname = "", hwid = "", subscription = "true" },
     {username = "ch111", password = "ch111", discord_nickname = "trlessless", hwid = "B189165D-B889-4FD5-BE1A-AB0B6C4FE674", subscription = "true"},
-    {username = "admin", password = "admin123", discord_nickname = "love_gitler", hwid = "95DC1493-4ED1-436F-B563-CC545354CD8B", subscription = "true" }
+    {username = "admin", password = "admin123", discord_nickname = "love_gitler", hwid = "95DC1493-4ED1-436F-B563-CC545354CD8B", subscription = "true" },
+    {username = "pidor", password = "12345678", discord_nickname = "byblik_v_smetane", hwid = "", subscription = "false"}
 }
 
 
